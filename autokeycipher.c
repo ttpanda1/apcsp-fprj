cb0293511031248f00ff04db74629193a597002c
Daniel Zhou <zhoud3713@my.sduhsd.net>
2020-01-15 13:45:11 -0800

    added autokeycipher.c

---
+++ autokeycipher.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char table[26][26];
+
+void table()
+{
+  for ( i = 0; i < 26; i++ )
+  {
+    printf("Hello world\n");
+  }
+}
+
+int main()
+{
+  printf("Hello world\n");
+}
