2ea82e2a7a02bb478d2701f58977553ad3a4d8ce
Kenneth Zhang <zhangk3714@my.sduhsd.net>
2020-01-21 13:23:21 -0800

    not working

--- vigenerecipher.c
+++ vigenerecipher.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-char key[] = "encrypt";
 char keystring[200];
 int k;
-char charstring[] = "testingthiscode";
 char output[200];
+char charstring[200];
 
-int main() {
+int main(int argc, char* argv[]) {
+
+if (argc >= 4) {
+  for ( int i = 2; argc > i; i++) {
+    strcat(charstring, argv[i]);
+    strcat(charstring, " ");
+  }
+  printf("%s\n", charstring);
+}
+else {
+  char* charstring = argv[2];
+}
+
+char* key = argv[1];
+
   for ( int i = 0; strlen(charstring) > i; i++) {
     k = i % strlen(key);
     keystring[i] = key[k];
   }
+  printf("%s\n", keystring);
+
+  for ( int i = 0; strlen(keystring) > i; i++) {
+    if (charstring[i] == ' ') {
+      for ( int b = strlen(keystring); b == i++; b - 1) {
+        int a = b - 1;
+        keystring[b] = keystring[a];
+      }
+      int t = i - 1;
+      keystring[t] = ' ';
+      printf("%s\n", keystring);
+    }
+  }
+  printf("%s\n", keystring);
 
   for ( int i = 0; strlen(charstring) > i; i++) {
-    char tm = charstring[i] + keystring[i] - 194;
-    int tmp = tm % 26;
-    output[i] = 'a' + tmp;
-    printf("%c", output[i]);
+     if (charstring[i] != ' ') {
+      char tm = charstring[i] + keystring[i] - 194;
+      int tmp = tm % 26;
+      output[i] = 'a' + tmp;
+      printf("%c", output[i]);
+    }
+
   }
   printf("\n");
 }
